a9a0a537227c37d8cb37edb6dd7429b0817b517d
安光 <1713862733@qq.com>
2018-10-11 21:03:33 +0800

    feat: 0349-Intersection-of-Two-Arrays solved.

---
+++ 0349-Intersection-of-Two-Arrays/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                // 这里仍然用 set 是为了除去 nums2 中的重复数值
+                set2.add(num);
+            }
+        }
+        int[] res = new int[set2.size()];
+        int i = 0;
+        for (int num : set2) {
+            res[i++] = num;
+        }
+        return res;
+    }
+}
