155d8b640cfe547c0d238e19bd0b836ed6d63440
安光 <1713862733@qq.com>
2018-10-29 16:10:31 +0800

    feat: 0283-Move-Zeroes solved.

--- 0283-Move-Zeroes/src/Solution.java
+++ 0283-Move-Zeroes/src/Solution.java
@@ -2,6 +2,35 @@ import java.util.Arrays;
 
 class Solution {
     public void moveZeroes(int[] nums) {
+        // nums 中，[0,k)的元素为非 0 元素
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j++] = temp;
+                } else {
+                    j++;
+                }
+            }
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+    public void moveZeroes3(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
