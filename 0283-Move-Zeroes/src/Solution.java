9ac85d3c8936003dc39f2bd3cf67a08210615d7a
安光 <1713862733@qq.com>
2018-10-29 15:11:32 +0800

    feat: 0283-Move-Zeroes solved.

---
+++ 0283-Move-Zeroes/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        solution.moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
