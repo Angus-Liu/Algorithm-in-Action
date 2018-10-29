95dc6950bfc5b8bffb578b2aa37416c8677690af
安光 <1713862733@qq.com>
2018-10-29 20:42:34 +0800

    feat: 0345-Reverse-Vowels-of-a-String solved.

---
+++ 0345-Reverse-Vowels-of-a-String/src/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String reverseVowels(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        // 对撞指针实现
+        int l = 0;
+        int r = s.length() - 1;
+        char[] chars = s.toCharArray();
+        while (l < r) {
+            char lch = chars[l];
+            char rch = chars[r];
+            if (isVowel(lch)) {
+                if (isVowel(rch)) {
+                    char temp = chars[l];
+                    chars[l] = chars[r];
+                    chars[r] = temp;
+                    l++;
+                    r--;
+                } else {
+                    r--;
+                }
+            } else {
+                l++;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseVowels("hello"));
+        System.out.println(solution.reverseVowels("leetcode"));
+    }
+}
