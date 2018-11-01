bfee10f17287c3b11a2c9e4fc8eb29bef827dbe1
安光 <1713862733@qq.com>
2018-11-01 10:39:49 +0800

    feat: add 0018-4-Sum.

---
+++ 0018-4-Sum/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        // 先对 nums 做预处理
+        Arrays.sort(nums);
+
+        return null;
+    }
+}
