39bdfba64298c337569deb42dfe9f22f04182ed5
安光 <1713862733@qq.com>
2018-11-01 11:05:42 +0800

    feat: 0049-Group-Anagrams solved.

--- 0049-Group-Anagrams/src/Solution.java
+++ 0049-Group-Anagrams/src/Solution.java
@@ -1,8 +1,27 @@
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // TODO: 查找表相关问题
-        return null;
+        // 查找表法
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            // 将每个字符串对应字符数组经排序后得到的字符串作为该字符串的分类标志
+            String flag = String.valueOf(chars);
+            List<String> list = map.getOrDefault(flag, new ArrayList<>());
+            list.add(str);
+            map.put(flag, list);
+        }
+        List<List<String>> res = new ArrayList<>();
+        for (String flag : map.keySet()) {
+            res.add(map.get(flag));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
     }
 }
