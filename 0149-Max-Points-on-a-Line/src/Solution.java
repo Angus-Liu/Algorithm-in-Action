b02b16f27c82ddec67886879d5484858474bc7ca
安光 <1713862733@qq.com>
2018-11-01 10:45:27 +0800

    feat: add 0149-Max-Points-on-a-Line.

---
+++ 0149-Max-Points-on-a-Line/src/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for a point.
+ * class Point {
+ * int x;
+ * int y;
+ * Point() { x = 0; y = 0; }
+ * Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+class Solution {
+    public int maxPoints(Point[] points) {
+        // TODO: 查找表法
+        return 0;
+    }
+}
