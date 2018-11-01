86716d8b7f842b3202694793d1fc31127edcbe28
安光 <1713862733@qq.com>
2018-11-01 15:56:06 +0800

    feat: 0083-Remove-Duplicates-from-Sorted-List solved.

---
+++ 0083-Remove-Duplicates-from-Sorted-List/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 3, 4, 4};
+        ListNode head = NodeUtil.createList(arr);
+        head = new Solution().deleteDuplicates(head);
+        NodeUtil.printList(head);
+    }
+}
