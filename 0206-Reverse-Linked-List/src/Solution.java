e47e65a8c50315be66456ac6d134e812a2953cae
安光 <1713862733@qq.com>
2018-11-01 15:22:04 +0800

    feat: 0206-Reverse-Linked-List solved.

--- 0206-Reverse-Linked-List/src/Solution.java
+++ 0206-Reverse-Linked-List/src/Solution.java
@@ -1,12 +1,18 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
+    /**
+     * 递归实现
+     *
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         // 递归到最后一个节点
         if (head == null || head.next == null) {
@@ -19,5 +25,26 @@ class Solution {
         // 防止在原头节点处成环
         head.next = null;
         return newHead;
+    }
+
+    /**
+     * 借助三个指针实现
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        // 递归到最后一个节点
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            // 进行变换
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 }
