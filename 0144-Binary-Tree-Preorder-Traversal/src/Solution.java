a9918dc3b16dfa0bfc23a479a46981873ba96778
安光 <1713862733@qq.com>
2018-11-02 15:48:22 +0800

    feat: 0144-Binary-Tree-Preorder-Traversal solved.

--- 0144-Binary-Tree-Preorder-Traversal/src/Solution.java
+++ 0144-Binary-Tree-Preorder-Traversal/src/Solution.java
@@ -5,6 +5,12 @@ class Solution {
 
     List<Integer> list = new ArrayList<>();
 
+    /**
+     * 递归算法，若非递归需借助栈实现
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         if (root != null) {
             list.add(root.val);
