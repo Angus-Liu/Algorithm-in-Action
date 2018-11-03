1a03fe883ac30571b2f16def3dc5db6c67e6f0e9
安光 <1713862733@qq.com>
2018-11-03 10:23:14 +0800

    feat: 0104-Maximum-Depth-of-Binary-Tree solved.

--- 0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
+++ 0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
@@ -1,21 +1,20 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
-        } else {
-            int leftDepth = maxDepth(root.left);
-            int rightDepth = maxDepth(root.right);
-            return leftDepth > rightDepth? ++leftDepth : ++rightDepth;
         }
+//        int leftDepth = maxDepth(root.left);
+//        int rightDepth = maxDepth(root.right);
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     public static void main(String[] args) {
