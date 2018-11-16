c1d575673d77be085f13c4d5701cd6ac8a9d4337
安光 <1713862733@qq.com>
2018-11-16 10:14:13 +0800

    feat: 0092-Reverse-Linked-List-II solved.

--- 0092-Reverse-Linked-List-II/src/Solution.java
+++ 0092-Reverse-Linked-List-II/src/Solution.java
@@ -8,6 +8,37 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        // 最终为 m 位置的前一点
+        ListNode mPre = dummyHead;
+        // 最终为 n 位置的后一点
+        ListNode nNext;
+        // 待反转的两点
+        ListNode cur;
+        ListNode next;
+        for (int i = 0; i < m - 1; i++) {
+            mPre = mPre.next;
+        }
+        cur = mPre.next;
+        next = cur.next;
+        nNext = next;
+        for (int i = 0; i < n - m; i++) {
+            // 进行反转
+            nNext = next.next;
+            next.next = cur;
+            // 移动到下一个反转处
+            cur = next;
+            next = nNext;
+        }
+        // 反转完毕，需要将两端与 mPre 和 nNext 连接
+        mPre.next.next = nNext;
+        mPre.next = cur;
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(new int[]{1, 2, 3, 4, 5});
+        System.out.println(new Solution().reverseBetween(head, 1, 5));
     }
 }
