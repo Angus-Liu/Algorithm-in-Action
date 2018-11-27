b20af330dd8fc27eef0ef7978b33460dc1c0ab56
安光 <1713862733@qq.com>
2018-11-27 08:10:02 +0800

    feat: 0230-Kth-Smallest-Element-in-a-BST solved.

--- 0230-Kth-Smallest-Element-in-a-BST/src/Solution.java
+++ 0230-Kth-Smallest-Element-in-a-BST/src/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
+
+    private int index;
+
     public int kthSmallest(TreeNode root, int k) {
-        return 0;
+        // 二分查找，中序遍历
+        int res = 0;
+        if (root == null) {
+            return res;
+        }
+        res = kthSmallest(root.left, k);
+        if (index == k) {
+            return res;
+        }
+        if (++index == k) {
+            return root.val;
+        }
+        return kthSmallest(root.right, k);
     }
 }
