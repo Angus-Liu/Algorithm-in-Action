614bf03f95528ae471f8e5e6466a59073de58d33
安光 <1713862733@qq.com>
2018-11-29 08:25:42 +0800

    feat: 0617-Merge-Two-Binary-Trees solved.

---
+++ 0617-Merge-Two-Binary-Trees/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        // 先合并根节点
+        t1.val += t2.val;
+        // 再递归合并左右子树
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
