18be33fa90fee8142e5a42b938ba1201f660047b
安光 <1713862733@qq.com>
2018-12-05 07:45:47 +0800

    feat: 0494-Target-Sum solved.

--- 0494-Target-Sum/src/Solution.java
+++ 0494-Target-Sum/src/Solution.java
@@ -1,6 +1,21 @@
 class Solution {
+
     public int findTargetSumWays(int[] nums, int S) {
-        // 记忆化搜索 -> 动态规划
-        return 0;
+        return findTargetSumWays(nums, 0, S, 0);
+    }
+
+    private int findTargetSumWays(int[] nums, int index, int S, int sum) {
+        int res = 0;
+        if (index == nums.length) {
+            return sum == S ? ++res : res;
+        }
+        res += findTargetSumWays(nums, index + 1, S, sum + nums[index]);
+        res += findTargetSumWays(nums, index + 1, S, sum - nums[index]);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
     }
 }
