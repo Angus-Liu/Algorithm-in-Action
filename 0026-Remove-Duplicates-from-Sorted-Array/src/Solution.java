6f5c23383cd800185d146e36d1c614778c4144c9
安光 <1713862733@qq.com>
2018-12-14 07:19:44 +0800

    feat: 0026-Remove-Duplicates-from-Sorted-Array solved.

--- 0026-Remove-Duplicates-from-Sorted-Array/src/Solution.java
+++ 0026-Remove-Duplicates-from-Sorted-Array/src/Solution.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // 双指针
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != nums[j]) {
