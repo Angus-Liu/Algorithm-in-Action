42526e6c5f42b3d38068bf4fa113fa1e42e2d8fd
安光 <1713862733@qq.com>
2018-12-16 15:35:49 +0800

    feat: 0958-Check-Completeness-of-a-Binary-Tree solved.

--- 0958-Check-Completeness-of-a-Binary-Tree/src/Solution.java
+++ 0958-Check-Completeness-of-a-Binary-Tree/src/Solution.java
@@ -2,8 +2,7 @@ import java.util.LinkedList;
 
 class Solution {
     /**
-     * 判定一棵树是不是完全二叉树的思路是广度遍历该二叉树，当出现NULL值时停止遍历，
-     * 如果此时还有没有遍历到的结点，那么就说明该树非完全二叉树，因为有空洞。
+     * 广度遍历二叉树，当出现 null 值时停止遍历，如果此时还有没有遍历到的结点，那么就说明该树非完全二叉树。
      */
     public boolean isCompleteTree(TreeNode root) {
         LinkedList<TreeNode> q = new LinkedList<>();
