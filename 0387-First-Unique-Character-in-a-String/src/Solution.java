0e5eb722db35f7caf8c22594d48c5787e51c3578
安光 <1713862733@qq.com>
2018-12-18 09:30:02 +0800

    feat: 0387-First-Unique-Character-in-a-String solved.

--- 0387-First-Unique-Character-in-a-String/src/Solution.java
+++ 0387-First-Unique-Character-in-a-String/src/Solution.java
@@ -1,35 +1,17 @@
 import java.util.HashMap;
-import java.util.Map;
+        import java.util.Map;
 
 /**
  * @author Angus
  * @date 2018/10/21
  */
 class Solution {
-    public int firstUniqChar(String s) {
 
-        int[] freq = new int[26];
-        char[] chars = s.toCharArray();
-        for (char ch : chars) {
-            freq[ch - 'a']++;
-        }
-        for(int i = 0; i < chars.length; i++) {
-            if (freq[chars[i] - 'a'] == 1) {
-                return i;
-            }
-        }
-        return -1;
-
-
-        /*
-        if (s == null) {
-            return -1;
-        }
+    public int firstUniqChar_1(String s) {
         Map<Character, Integer> map = new HashMap<>(26);
         char[] chars = s.toCharArray();
         for (char ch : chars) {
-            Integer count = map.get(ch);
-            map.put(ch, count == null ? 1 : count + 1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         for (int i = 0; i < chars.length; i++) {
             if (map.get(chars[i]) == 1) {
@@ -37,7 +19,56 @@ class Solution {
             }
         }
         return -1;
-        */
+    }
+
+    public int firstUniqChar_2(String s) {
+        int[] freq = new int[26];
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            freq[ch - 'a']++;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (freq[chars[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int firstUniqChar_3(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (s.indexOf(ch) == s.lastIndexOf(ch)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int firstUniqChar_4(String s) {
+        boolean[] notUniq = new boolean[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!notUniq[ch -'a']) {
+                if (s.indexOf(ch) == s.lastIndexOf(ch)) {
+                    return i;
+                } else {
+                    notUniq[ch - 'a'] = true;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int firstUniqChar(String s) {
+        int res = -1;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int index = s.indexOf(ch);
+            if (index != -1 && index == s.lastIndexOf(ch)) {
+                res = (res == -1 || res > index) ? index : res;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
