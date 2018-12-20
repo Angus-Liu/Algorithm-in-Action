159676a22c81417dbb3c12acd2f0eadf93dfc541
安光 <1713862733@qq.com>
2018-12-20 09:25:26 +0800

    feat: 0104-Maximum-Depth-of-Binary-Tree solved.

--- 0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
+++ 0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
@@ -9,12 +9,7 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-//        int leftDepth = maxDepth(root.left);
-//        int rightDepth = maxDepth(root.right);
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     public static void main(String[] args) {
