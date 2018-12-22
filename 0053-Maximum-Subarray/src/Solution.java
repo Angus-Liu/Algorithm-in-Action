8318e31382a87b796b1699465270221e50b97f42
安光 <1713862733@qq.com>
2018-12-22 09:31:50 +0800

    feat: 0053-Maximum-Subarray solved.

---
+++ 0053-Maximum-Subarray/src/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            // 正值继续传递，负值及时终止
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+}
