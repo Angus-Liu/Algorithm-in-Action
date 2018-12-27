f4cce29be5334e0554df50488225d70f37259371
安光 <1713862733@qq.com>
2018-12-27 09:19:32 +0800

    feat: 0349-Intersection-of-Two-Arrays solved.

--- LeetCode-in-Action/0349-Intersection-of-Two-Arrays/src/Solution.java
+++ LeetCode-in-Action/0349-Intersection-of-Two-Arrays/src/Solution.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersection_1(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
@@ -17,6 +18,24 @@ class Solution {
         int[] res = new int[set2.size()];
         int i = 0;
         for (int num : set2) {
+            res[i++] = num;
+        }
+        return res;
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
+        }
+        set1.retainAll(set2);
+        int[] res = new int[set1.size()];
+        int i = 0;
+        for (int num : set1) {
             res[i++] = num;
         }
         return res;
