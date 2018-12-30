ef92487602dd73b5b716fe39868c8e1667325c24
安光 <1713862733@qq.com>
2018-12-30 18:58:32 +0800

    feat: 0966-Vowel-Spellchecker solved.

--- LeetCode-in-Action/0966-Vowel-Spellchecker/src/Solution.java
+++ LeetCode-in-Action/0966-Vowel-Spellchecker/src/Solution.java
@@ -1,77 +1,58 @@
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        List<String> wordList = Arrays.asList(wordlist);
+        Map<String, String> wordMap = new HashMap<>();
+        Map<String, String> wordPatternMap = new HashMap<>();
+        for (String word : wordlist) {
+            wordMap.put(word, word);
+            wordPatternMap.putIfAbsent(word.toLowerCase(), word);
+            wordPatternMap.putIfAbsent(wordPattern(word), word);
+        }
+        // System.out.println(wordMap);
+        // System.out.println(wordPatternMap);
         for (int i = 0; i < queries.length; i++) {
-            boolean similar = false;
-            for (int j = 0; j < wordlist.length; j++) {
-                if (wordList.contains(queries[i])) {
-                    similar = true;
-                    break;
-                }
-
-                if (similarVowel(queries[i], wordlist[j])) {
-                    queries[i] = wordlist[j];
-                    similar = true;
-                    break;
-                }
-
-                if (similar(queries[i].toLowerCase(), wordlist[j].toLowerCase())) {
-                    queries[i] = wordlist[j];
-                    similar = true;
-                    break;
-                }
+            if (wordMap.containsKey(queries[i])) {
+                queries[i] = wordMap.get(queries[i]);
+                continue;
             }
-            if (!similar) {
-                queries[i] = "";
+            String wordPattern = queries[i].toLowerCase();
+            if (wordPatternMap.containsKey(wordPattern)) {
+                queries[i] = wordPatternMap.get(wordPattern);
+                continue;
             }
+            wordPattern = wordPattern(queries[i]);
+            if (wordPatternMap.containsKey(wordPattern)) {
+                queries[i] = wordPatternMap.get(wordPattern);
+                continue;
+            }
+            queries[i] = "";
         }
         return queries;
     }
 
-    private boolean similarVowel(String strA, String strB) {
-        if (strA.length() != strB.length()) {
-            return false;
-        }
-        for (int i = 0; i < strA.length(); i++) {
-            char chA = strA.charAt(i);
-            char chB = strB.charAt(i);
-            if (isVowel(chA) && isVowel(chB)) {
-                continue;
-            }
-            if (chA != chB) {
-                return false;
+    private String wordPattern(String word) {
+        // return word.toLowerCase().replaceAll("a|e|i|o|u", "*");
+        char[] letters = word.toLowerCase().toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == 'a'
+                    || letters[i] == 'e'
+                    || letters[i] == 'i'
+                    || letters[i] == 'o'
+                    || letters[i] == 'u') {
+                letters[i] = '*';
             }
         }
-        return true;
-    }
-
-    private boolean similar(String strA, String strB) {
-        if (strA.length() != strB.length()) {
-            return false;
-        }
-        for (int i = 0; i < strA.length(); i++) {
-            char chA = strA.charAt(i);
-            char chB = strB.charAt(i);
-            if (chA != chB) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
-                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+        return new String(letters);
     }
 
     public static void main(String[] args) {
-        String[] wordList = {"KiTe", "kite", "hare", "Hare"};
+        String[] wordlist = {"KiTe", "kite", "hare", "Hare"};
         String[] queries = {"kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"};
-        String[] res = new Solution().spellchecker(wordList, queries);
+        String[] res = new Solution().spellchecker(wordlist, queries);
         System.out.println(Arrays.toString(res));
     }
 }
