11534c63c4cce78e8d3f8bfd2b7f801f5f06f317
安光 <1713862733@qq.com>
2018-12-31 08:36:03 +0800

    feat: 0494-Target-Sum solved.

--- LeetCode-in-Action/0494-Target-Sum/src/Solution.java
+++ LeetCode-in-Action/0494-Target-Sum/src/Solution.java
@@ -1,6 +1,9 @@
 class Solution {
 
-    public int findTargetSumWays(int[] nums, int S) {
+    /**
+     * DFS
+     */
+    public int findTargetSumWays_1(int[] nums, int S) {
         return findTargetSumWays(nums, 0, S, 0);
     }
 
@@ -12,6 +15,29 @@ class Solution {
         res += findTargetSumWays(nums, index + 1, S, sum + nums[index]);
         res += findTargetSumWays(nums, index + 1, S, sum - nums[index]);
         return res;
+    }
+
+    /**
+     * DP
+     * https://leetcode.com/problems/target-sum/discuss/97334/Java-(15-ms)-C%2B%2B-(3-ms)-O(ns)-iterative-DP-solution-using-subset-sum-with-explanation
+     */
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1);
+    }
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+        for (int n : nums) {
+            for (int i = s; i >= n; i--) {
+                dp[i] += dp[i - n];
+            }
+        }
+        return dp[s];
     }
 
     public static void main(String[] args) {
