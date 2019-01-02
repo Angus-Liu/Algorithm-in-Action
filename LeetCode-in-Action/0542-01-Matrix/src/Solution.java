1a28d19e056531c21eb194a07ec89aca5a1f62cf
安光 <1713862733@qq.com>
2019-01-02 20:01:11 +0800

    feat: 0542-01-Matrix solved.

--- LeetCode-in-Action/0542-01-Matrix/src/Solution.java
+++ LeetCode-in-Action/0542-01-Matrix/src/Solution.java
@@ -1,41 +1,150 @@
 import java.util.Arrays;
+        import java.util.LinkedList;
+        import java.util.Queue;
 
 class Solution {
-
     private int row;
     private int col;
-    private boolean[][] visited;
+    private int[][] vector = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    public int[][] updateMatrix(int[][] matrix) {
+    /**
+     * DP（两次遍历，可 AC）
+     */
+    public int[][] updateMatrix_1(int[][] matrix) {
         row = matrix.length;
         col = matrix[0].length;
-        visited = new boolean[row][col];
-        updateMatrix(matrix, 0,0);
+        // 第一次遍历，正向遍历，根据相邻左元素和上元素得出当前元素的对应结果
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = row + col;
+                }
+                if (i > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j - 1] + 1);
+                }
+            }
+        }
+        // 第二次遍历，反向遍历，根据相邻右元素和下元素及当前元素的结果得出最终结果
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                if (i < row - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i + 1][j] + 1);
+                }
+                if (j < col - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j + 1] + 1);
+                }
+            }
+        }
         return matrix;
     }
 
 
-    private int updateMatrix(int[][] matrix, int x, int y) {
-        visited[x][y] = true;
-        int res = Integer.MAX_VALUE;
-        if (matrix[x][y] == 0) {
-            res = 0;
+    /**
+     * BFS（迭代实现，可 AC）
+     */
+    public int[][] updateMatrix_2(int[][] matrix) {
+        row = matrix.length;
+        col = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    // 将所有 0 元素作为 BFS 第一层
+                    queue.add(new int[]{i, j});
+                } else {
+                    matrix[i][j] = row + col;
+                }
+            }
         }
-        if (x - 1 >= 0 && !visited[x - 1][y]) {
-            res = Math.min(res, updateMatrix(matrix, x - 1, y) + 1);
+        while (!queue.isEmpty()) {
+            int[] s = queue.poll();
+            // 搜索上下左右四个方向
+            for (int[] v : vector) {
+                int r = s[0] + v[0], c = s[1] + v[1];
+                if (r >= 0 && r < row
+                        && c >= 0 && c < col
+                        && matrix[r][c] > matrix[s[0]][s[1]] + 1) {
+                    matrix[r][c] = matrix[s[0]][s[1]] + 1;
+                    queue.add(new int[]{r, c});
+                }
+            }
         }
-        if (x + 1 < row && !visited[x + 1][y]) {
-            res = Math.min(res, updateMatrix(matrix, x + 1, y) + 1);
-        }
-        if (y - 1 >= 0 && !visited[x][y - 1]) {
-            res = Math.min(res, updateMatrix(matrix, x, y - 1) + 1);
-        }
-        if (y + 1 < col && !visited[x][y + 1]) {
-            res = Math.min(res, updateMatrix(matrix, x, y + 1) + 1);
-        }
-        matrix[x][y] = res;
-        return res;
+        return matrix;
     }
+
+
+    /**
+     * DFS（递归实现，解答正确，但是会超时，下方是优化版）
+     */
+    public int[][] updateMatrix_3(int[][] matrix) {
+        row = matrix.length;
+        col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = row + col;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    updateMatrix(matrix, i, j);
+                }
+            }
+        }
+        return matrix;
+    }
+
+
+    /**
+     * DFS（递归实现，优化版，可 AC）
+     */
+    public int[][] updateMatrix(int[][] matrix) {
+        row = matrix.length;
+        col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // 优化：如果元素在 0 附近，保留元素值 1，不在 0 附近，初始化为一个较大值
+                if (matrix[i][j] == 1
+                        && !((i > 0 && matrix[i - 1][j] == 0)
+                        || (i < row - 1 && matrix[i + 1][j] == 0)
+                        || (j > 0 && matrix[i][j - 1] == 0)
+                        || (j < col - 1 && matrix[i][j + 1] == 0))) {
+                    matrix[i][j] = row + col;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                // 优化：将元素值为 1 的点作为深搜起点，降低递归深度
+                if (matrix[i][j] == 1) {
+                    updateMatrix(matrix, i, j);
+                }
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * DFS 递归函数
+     */
+    private void updateMatrix(int[][] matrix, int r, int c) {
+        // 搜索上下左右四个方向
+        for (int[] v : vector) {
+            int nr = r + v[0], nc = c + v[1];
+            if (nr >= 0 && nr < row
+                    && nc >= 0 && nc < col
+                    && matrix[nr][nc] > matrix[r][c] + 1) {
+                matrix[nr][nc] = matrix[r][c] + 1;
+                updateMatrix(matrix, nr, nc);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
@@ -43,7 +152,7 @@ class Solution {
                 {0, 1, 0},
                 {1, 1, 1}
         };
-        matrix = new Solution().updateMatrix(matrix);
+        matrix = new Solution().updateMatrix_3(matrix);
         for (int[] row : matrix) {
             System.out.println(Arrays.toString(row));
         }
