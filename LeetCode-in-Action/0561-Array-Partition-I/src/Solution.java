2d6a91835246ca0ce54cb3f4dcc5b360089e382e
安光 <1713862733@qq.com>
2019-01-08 09:48:57 +0800

    feat: 0561-Array-Partition-I solved.

---
+++ LeetCode-in-Action/0561-Array-Partition-I/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0; i < len; i += 2) {
+            res += nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().arrayPairSum(new int[]{1, 4, 3, 2}));
+    }
+}
