4dae5657ff89c23e84f016677cde1a4faef174e3
安光 <1713862733@qq.com>
2019-01-15 09:50:44 +0800

    feat: 0153-Find-Minimum-in-Rotated-Sorted-Array solved.

---
+++ LeetCode-in-Action/0153-Find-Minimum-in-Rotated-Sorted-Array/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= nums[l] && nums[m] <= nums[r]) {
+                return nums[l];
+            } else if (nums[m] < nums[l]) {
+                // 注意，不能直接赋值为 m + 1，因原 m 可能为最终答案
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}
