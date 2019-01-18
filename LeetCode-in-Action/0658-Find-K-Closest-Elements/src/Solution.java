81bb2a855a5f3b554e2a3e845e2ced0a09912040
安光 <1713862733@qq.com>
2019-01-18 09:35:20 +0800

    feat: 0658-Find-K-Closest-Elements solved.

--- LeetCode-in-Action/0658-Find-K-Closest-Elements/src/Solution.java
+++ LeetCode-in-Action/0658-Find-K-Closest-Elements/src/Solution.java
@@ -2,40 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    /**
-     * 分为 x 在 arr 中和不在 arr 中进行讨论
-     */
+
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> res = new ArrayList<>(k);
-        if (x <= arr[0]) {
-            for (int i = 0; i < k; i++) {
-                res.add(arr[i]);
+        int left = 0, right = arr.length - k;
+        // 利用二分法找到最接近 x 的数的左边界
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (Math.abs(arr[mid] - x) > Math.abs(arr[mid + k] - x)) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-        } else if (x >= arr[arr.length - 1]) {
-            for (int i = arr.length - k; i < arr.length; i++) {
-                res.add(arr[i]);
-            }
-        } else {
-            int l = 0, r = arr.length - 1, m = 0;
-            while (l <= r) {
-                m = l + (r - l) / 2;
-                if (arr[m] == x) {
-                    break;
-                } else if (arr[m] < x) {
-                    l = m + 1;
-                } else {
-                    r = m - 1;
-                }
-            }
-
         }
-
-
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            res.add(arr[i]);
+        }
         return res;
     }
 
     public static void main(String[] args) {
-        List<Integer> res = new Solution().findClosestElements(new int[]{1, 2, 3, 4, 5}, 4, 3);
+        List<Integer> res = new Solution().findClosestElements(new int[]{1, 2, 3, 4, 5}, 4, -1);
         System.out.println(res);
     }
 }
