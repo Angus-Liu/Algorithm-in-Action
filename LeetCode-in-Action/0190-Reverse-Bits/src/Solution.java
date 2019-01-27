616c5fb57c92bb13748673b6477a41d7ac27b43f
安光 <1713862733@qq.com>
2019-01-27 13:28:49 +0800

    chore: rename LeetCode-in-Action to LeetCode.

--- LeetCode-in-Action/0190-Reverse-Bits/src/Solution.java
+++
@@ -1,6 +0,0 @@
-public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-       return Integer.reverse(n);
-    }
-}
--- LeetCode-in-Action/0287-Find-the-Duplicate-Number/src/Solution.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public int findDuplicate(int[] nums) {
-        return -1;
-    }
-}
