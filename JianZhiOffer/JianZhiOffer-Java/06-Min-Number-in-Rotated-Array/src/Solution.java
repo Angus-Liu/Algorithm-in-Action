c65d05b094c65bc368de5d2cfe8d67c048b86aca
安光 <1713862733@qq.com>
2019-02-01 13:59:31 +0800

    feat: 06-Min-Number-in-Rotated-Array solved.

---
+++ JianZhiOffer/JianZhiOffer-Java/06-Min-Number-in-Rotated-Array/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minNumberInRotateArray(int[] array) {
+        return binarySearch(array, 0, array.length - 1);
+    }
+
+    private int binarySearch(int[] array, int left, int right) {
+        int mid = left + (right - left) / 2;
+        if (array[mid] >= array[left] && array[mid] <= array[right]) {
+            return array[left];
+        } else if (array[mid] < array[left]) {
+            return binarySearch(array, left, mid);
+        } else {
+            return binarySearch(array, mid + 1, right);
+        }
+    }
+}
