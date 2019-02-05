b834c76a7d7dc48fe9e176a5a948803d6390e793
安光 <1713862733@qq.com>
2019-02-05 09:40:26 +0800

    feat: 11-Number-of-1-in-Binary solved.

---
+++ JianZhiOffer/JianZhiOffer-Java/11-Number-of-1-in-Binary/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int NumberOf1(int n) {
+        int count = 0;
+        int flag = 1;
+        while (flag != 0) {
+            if ((n & flag) != 0) {
+                count++;
+            }
+            flag <<= 1;
+        }
+        return count;
+    }
+}
