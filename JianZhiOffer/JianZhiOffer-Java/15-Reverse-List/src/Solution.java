2fd5322b2b0ddc9ca04135f911aba68d7dd5f50f
安光 <1713862733@qq.com>
2019-02-09 09:20:51 +0800

    feat: 15-Reverse-List solved.

---
+++ JianZhiOffer/JianZhiOffer-Java/15-Reverse-List/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
