0c1d5677627a46fdcc35a4df91534ea6f3ad84af
安光 <1713862733@qq.com>
2019-02-17 22:24:12 +0800

    feat: 23-Sequence-of-BST solved.

--- JianZhiOffer/JianZhiOffer-Java/23-Sequence-of-BST/src/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/23-Sequence-of-BST/src/Solution.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.List;
-        import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public boolean VerifySquenceOfBST(int[] sequence) {
