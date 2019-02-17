6ae7db84384b9210d4400331d92cdc4ad0360f08
安光 <1713862733@qq.com>
2019-02-17 21:21:08 +0800

    feat: 23-Sequence-of-BST solved.

--- JianZhiOffer/JianZhiOffer-Java/23-Sequence-of-BST/src/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/23-Sequence-of-BST/src/Solution.java
@@ -1,5 +1,43 @@
-public class Solution {
-    public boolean VerifySquenceOfBST(int [] sequence) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Solution {
+    public boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0) {
+            return false;
+        }
+        return verifySquence(Arrays.stream(sequence).boxed().collect(Collectors.toList()));
+    }
+
+    private boolean verifySquence(List<Integer> seq) {
+        if (seq.size() == 0) {
+            return true;
+        }
+        // 序列的最后一个节点为该树的根节点
+        int root = seq.get(seq.size() - 1);
+        // 左子树所有节点小于根节点
+        List<Integer> leftSeq = new ArrayList<>();
+        int i = 0;
+        while (i < seq.size() - 1 && seq.get(i) < root) {
+            leftSeq.add(seq.get(i++));
+        }
+        // 右子树所有节点大于根节点
+        List<Integer> rightSeq = new ArrayList<>();
+        while (i < seq.size() - 1 && seq.get(i) > root) {
+            rightSeq.add(seq.get(i++));
+        }
+        // 右子树中有节点小于根节点，说明不是合法序列
+        if (i != seq.size() - 1) {
+            return false;
+        }
+        return verifySquence(leftSeq) && verifySquence(rightSeq);
+    }
+
+    public static void main(String[] args) {
+        int[] sequence = {4, 8, 6, 12, 16, 14, 10};
+        boolean res = new Solution().VerifySquenceOfBST(sequence);
+        System.out.println(res);
     }
 }
