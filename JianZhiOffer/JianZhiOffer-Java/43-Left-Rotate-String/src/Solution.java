6f5388c485f4e01c981922d268abdaa6786db5d8
安光 <1713862733@qq.com>
2019-03-09 07:54:02 +0800

    feat: 43-Left-Rotate-String solved.

--- JianZhiOffer/JianZhiOffer-Java/43-Left-Rotate-String/src/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/43-Left-Rotate-String/src/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String LeftRotateString(String str, int n) {
-        if (str == null || str.length() == 0) return "";
+        if (str == null || str.length() == 0) return str;
         char[] chArr = str.toCharArray();
         n %= chArr.length;
         reverse(chArr, 0, chArr.length - 1);
