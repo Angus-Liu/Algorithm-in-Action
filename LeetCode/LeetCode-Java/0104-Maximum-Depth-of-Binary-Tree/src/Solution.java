2799b7acd51d5b7bc3790b6e767e1ec40d6ad08e
安光 <1713862733@qq.com>
2019-03-12 08:14:21 +0800

    feat: 0104-Maximum-Depth-of-Binary-Tree solved.

--- LeetCode/LeetCode-Java/0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
+++ LeetCode/LeetCode-Java/0104-Maximum-Depth-of-Binary-Tree/src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,8 +11,51 @@
  * }
  */
 class Solution {
+
+    /**
+     * 递归（自顶向下）
+     */
+    private int maxDepth;
+
     public int maxDepth(TreeNode root) {
-        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        topDown(root, 0);
+        return maxDepth;
+    }
+
+    private void topDown(TreeNode node, int depth) {
+        if (node == null) {
+            maxDepth = depth > maxDepth ? depth : maxDepth;
+            return;
+        }
+        topDown(node.left, depth + 1);
+        topDown(node.right, depth + 1);
+    }
+
+    /**
+     * 递归（自底向上）
+     */
+    public int maxDepth2(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth2(root.left), maxDepth2(root.right)) + 1;
+    }
+
+    /**
+     * 迭代
+     */
+    public int maxDepth3(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode n = queue.remove();
+                if (n.left != null) queue.add(n.left);
+                if (n.right != null) queue.add(n.right);
+            }
+        }
+        return level;
     }
 
     public static void main(String[] args) {
