4cf55f57da80c895ff83680ad50e808f28b58b79
安光 <1713862733@qq.com>
2019-03-13 08:01:14 +0800

    feat: 46-Last-Number-in-Circle solved.

---
+++ JianZhiOffer/JianZhiOffer-Java/46-Last-Number-in-Circle/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int LastRemaining_Solution(int n, int m) {
+        if (n < 1 || m < 1) {
+            return -1;
+        }
+        int last = 0;
+        for (int i = 2; i <= n; i++) {
+            last = (last + m) % i;
+        }
+        return last;
+    }
+}
