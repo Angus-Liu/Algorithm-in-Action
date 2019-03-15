d2e2dbe0f7d8eb6818578f006b78ff857f4fb711
安光 <1713862733@qq.com>
2019-03-15 10:15:55 +0800

    feat: 0106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal solved.

---
+++ LeetCode/LeetCode-Java/0106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/src/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] inorder, int is, int ie, int[] postorder, int ps, int pe) {
+        if (is > ie) {
+            return null;
+        }
+        // 后序遍历最后一个节点为根节点
+        int rootVal = postorder[pe];
+        TreeNode root = new TreeNode(rootVal);
+        // 确定根节点在中序遍历中的位置
+        int im = is;
+        while (im < ie && inorder[im] != rootVal) {
+            im++;
+        }
+        // 根据根节点在中序遍历中的位置得到左右子树的中序遍历和后序遍历序列
+        root.left = buildTree(inorder, is, im - 1, postorder, ps, ps + (im - 1 - is));
+        root.right = buildTree(inorder, im + 1, ie, postorder, ps + (im - is), pe - 1);
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        int[] inorder = {9, 3, 15, 20, 7};
+        int[] postorder = {9, 15, 7, 20, 3};
+        TreeNode tree = new Solution().buildTree(inorder, postorder);
+    }
+}
