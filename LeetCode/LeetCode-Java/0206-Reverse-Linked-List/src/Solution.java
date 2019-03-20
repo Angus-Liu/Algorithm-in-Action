0971dc25eeca0c1b6673abe197b106ad380a4f0b
安光 <1713862733@qq.com>
2019-03-20 10:07:01 +0800

    feat: 0206-Reverse-Linked-List solved.

--- LeetCode/LeetCode-Java/0206-Reverse-Linked-List/src/Solution.java
+++ LeetCode/LeetCode-Java/0206-Reverse-Linked-List/src/Solution.java
@@ -8,12 +8,9 @@
  */
 class Solution {
     /**
-     * 递归实现
-     *
-     * @param head
-     * @return
+     * 递归
      */
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList1(ListNode head) {
         // 递归到最后一个节点
         if (head == null || head.next == null) {
             return head;
@@ -28,10 +25,7 @@ class Solution {
     }
 
     /**
-     * 借助三个指针实现
-     *
-     * @param head
-     * @return
+     * 迭代：借助三个指针实现
      */
     public ListNode reverseList2(ListNode head) {
         ListNode pre = null;
@@ -45,5 +39,20 @@ class Solution {
             cur = next;
         }
         return pre;
+    }
+
+    /**
+     * 迭代：dummyHead + 头插法
+     */
+    public ListNode reverseList(ListNode head) {
+        // 虚拟头结点一开始并不连接到链表头结点（或者理解为构造了一条新的链表更为合适）
+        ListNode dummyHead = new ListNode(0);
+        while (head != null) {
+            ListNode cur = head;
+            head = head.next;
+            cur.next = dummyHead.next;
+            dummyHead.next = cur;
+        }
+        return dummyHead.next;
     }
 }
