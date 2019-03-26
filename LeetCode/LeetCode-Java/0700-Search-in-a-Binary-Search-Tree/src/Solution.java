1d9d0f27e95163034c8c11068573ea73016c515d
Angus-Liu <angus.liu96@gmail.com>
2019-03-26 08:31:02 +0800

    feat: 0700-Search-in-a-Binary-Search-Tree solved.

--- LeetCode/LeetCode-Java/0700-Search-in-a-Binary-Search-Tree/src/Solution.java
+++ LeetCode/LeetCode-Java/0700-Search-in-a-Binary-Search-Tree/src/Solution.java
@@ -3,8 +3,7 @@ class Solution {
      * 递归
      */
     public TreeNode searchBST_1(TreeNode root, int val) {
-        if (root == null) return null;
-        if (root.val == val) return root;
+        if (root == null || root.val == val) return root;
         return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
     }
 
@@ -12,11 +11,9 @@ class Solution {
      * 迭代
      */
     public TreeNode searchBST(TreeNode root, int val) {
-        while (root != null) {
-            if (root.val == val) return root;
-            if (root.val > val) root = root.left;
-            else root = root.right;
+        while (true) {
+            if (root == null || root.val == val) return root;
+            root = root.val > val ? root.left : root.right;
         }
-        return null;
     }
 }
