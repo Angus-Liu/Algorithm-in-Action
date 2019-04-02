679ea40a15d7001741f68da6585bcb7a3d5337f1
Angus-Liu <angus.liu96@gmail.com>
2019-04-02 20:50:20 +0800

    feat: 0589-N-ary-Tree-Preorder-Traversal solved.

--- LeetCode/LeetCode-Java/0589-N-ary-Tree-Preorder-Traversal/src/Solution.java
+++ LeetCode/LeetCode-Java/0589-N-ary-Tree-Preorder-Traversal/src/Solution.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-        import java.util.List;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 class Solution {
@@ -20,20 +21,36 @@ class Solution {
     }
 
     /**
-     * 非递归写法（借用栈实现）
+     * 迭代（借用栈实现）
      */
-    public List<Integer> preorderByStack(Node root) {
+    public List<Integer> preorder_2(Node root) {
         Stack<Node> stack = new Stack<>();
         if (root != null) {
             stack.add(root);
         }
-        while(!stack.empty()) {
+        while (!stack.empty()) {
             Node node = stack.pop();
             res.add(node.val);
             // 注意，这里是逆序，使得左孩子在栈顶
-            for (int i = node.children.size() -1; i >= 0; i--) {
+            for (int i = node.children.size() - 1; i >= 0; i--) {
                 stack.add(node.children.get(i));
             }
+        }
+        return res;
+    }
+
+    /**
+     * 迭代（队列当栈用）
+     */
+    public List<Integer> preorder_3(Node root) {
+        LinkedList<Node> queue = new LinkedList<>();
+        if (root != null) {
+            queue.addFirst(root);
+        }
+        while (!queue.isEmpty()) {
+            Node node = queue.removeFirst();
+            res.add(node.val);
+            queue.addAll(0, node.children);
         }
         return res;
     }
