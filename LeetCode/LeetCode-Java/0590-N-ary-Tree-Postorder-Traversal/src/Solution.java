a76ca8b0c598948a13c5e7041cd83b91f83577a9
Angus-Liu <angus.liu96@gmail.com>
2019-04-02 21:16:51 +0800

    feat: 0590-N-ary-Tree-Postorder-Traversal solved.

---
+++ LeetCode/LeetCode-Java/0590-N-ary-Tree-Postorder-Traversal/src/Solution.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+    private List<Integer> res = new ArrayList<>();
+
+    /**
+     * 递归实现
+     */
+    public List<Integer> postorder(Node root) {
+        if (root != null) {
+            for (Node child : root.children) {
+                postorder(child);
+            }
+            res.add(root.val);
+        }
+        return res;
+    }
+
+    /**
+     * 迭代实现（借助栈）
+     */
+    public List<Integer> postorder_2(Node root) {
+        LinkedList<Node> stack = new LinkedList<>();
+        if (root != null) {
+            stack.push(root);
+        }
+        while (!stack.isEmpty()) {
+            Node node = stack.peek();
+            if (node.children == null) {
+                res.add(stack.pop().val);
+            } else {
+                // for (int i = node.children.size() - 1; i >= 0; i--) {
+                //     stack.push(node.children.get(i));
+                // }
+                stack.addAll(0, node.children);
+                // 表示已遍历过其子节点（不太好，直接改变原节点信息了，但比较简单）
+                node.children = null;
+            }
+        }
+        return res;
+    }
+}
