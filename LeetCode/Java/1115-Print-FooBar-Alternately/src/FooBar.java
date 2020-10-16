b82dff5141d253918e2a4f235adbf421bd830307
liuanguang <liuanguang@cvte.com>
2020-10-16 09:48:17 +0800

    feat: 1115-Print-FooBar-Alternately solved

---
+++ LeetCode/Java/1115-Print-FooBar-Alternately/src/FooBar.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
+class FooBar {
+    private int n;
+
+    private final AtomicBoolean flag = new AtomicBoolean(true);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (!flag.get()) {
+                Thread.yield();
+            }
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            flag.set(false);
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (flag.get()) {
+                Thread.yield();
+            }
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            flag.set(true);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        FooBar fooBar = new FooBar(5);
+        new Thread(() -> {
+            try {
+                fooBar.foo(() -> System.out.print("foo"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                fooBar.bar(() -> System.out.print("bar"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+}
