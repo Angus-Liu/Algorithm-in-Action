10a25c0102d28d03dfbd2e8f9b87b80374a93f82
liuanguang <liuanguang@cvte.com>
2020-10-17 09:28:38 +0800

    feat: 1116-Print-Zero-Even-Odd solved

---
+++ LeetCode/Java/1116-Print-Zero-Even-Odd/src/ZeroEvenOdd.java
@@ -0,0 +1,78 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+    private int n;
+
+    private volatile int i = 1;
+    private Semaphore z = new Semaphore(1);
+    private Semaphore e = new Semaphore(0);
+    private Semaphore o = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            z.acquire();
+            if (i > n) {
+                e.release();
+                o.release();
+                return;
+            }
+            printNumber.accept(0);
+            if (i % 2 == 0)
+                e.release();
+            else
+                o.release();
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            e.acquire();
+            if (i > n) return;
+            printNumber.accept(i++);
+            z.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            o.acquire();
+            if (i > n) return;
+            printNumber.accept(i++);
+            z.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        ZeroEvenOdd zeo = new ZeroEvenOdd(5);
+
+        new Thread(() -> {
+            try {
+                zeo.zero(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                zeo.even(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                zeo.odd(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
