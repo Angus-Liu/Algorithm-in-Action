f00e3dfb12cedb32eb1fb60abcc14260248b5a59
liuanguang <liuanguang@cvte.com>
2020-10-18 13:36:13 +0800

    feat: add 1226-The-Dining-Philosophers

---
+++ LeetCode/Java/1226-The-Dining-Philosophers/src/DiningPhilosophers.java
@@ -0,0 +1,16 @@
+class DiningPhilosophers {
+
+    public DiningPhilosophers() {
+
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+
+    }
+}
