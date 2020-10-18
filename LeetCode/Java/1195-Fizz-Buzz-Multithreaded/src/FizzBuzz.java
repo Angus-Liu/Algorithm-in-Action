e133111e5ef6d9e4f4849ff4df4c95f5bf28bea1
liuanguang <liuanguang@cvte.com>
2020-10-18 13:09:53 +0800

    feat: add 1195-Fizz-Buzz-Multithreaded

---
+++ LeetCode/Java/1195-Fizz-Buzz-Multithreaded/src/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.function.IntConsumer;
+
+class FizzBuzz {
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
