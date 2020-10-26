89bfa7058444c98230ed24639fc6e1e1fc9798af
liuanguang <liuanguang@cvte.com>
2020-10-26 09:01:20 +0800

    feat: NO_0342_Power_of_Four solved

--- LeetCode/LeetCode-Java/src/main/java/NO_0342_Power_of_Four/Solution.java
+++ LeetCode/LeetCode-Java/src/main/java/NO_0342_Power_of_Four/Solution.java
@@ -23,11 +23,19 @@ public class Solution {
         return (num > 0) && ((num == 1) || (num % 4 == 0 && isPowerOfFour(num / 4)));
     }
 
+    public boolean isPowerOfFour3(int num) {
+        long n = 1;
+        while (n < num) {
+            n <<= 2;
+        }
+        return n == num;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {-1, 0, 1, 2, 4, 5, 16, 32, 64};
+        int[] nums = {-1, 0, 1, 2, 4, 5, 16, 32, 64, 1162261466};
         for (int num : nums) {
-            System.out.println(num + ": " + s.isPowerOfFour2(num));
+            System.out.println(num + ": " + s.isPowerOfFour3(num));
         }
     }
 }
