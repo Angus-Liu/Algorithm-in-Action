8cf784e019e422470f2c1e7a50b11260c3505382
liuanguang <liuanguang@cvte.com>
2020-11-02 08:50:18 +0800

    feat: NO_0349_Intersection_of_Two_Arrays solved

--- LeetCode/LeetCode-Java/src/main/java/NO_0349_Intersection_of_Two_Arrays/Solution.java
+++ LeetCode/LeetCode-Java/src/main/java/NO_0349_Intersection_of_Two_Arrays/Solution.java
@@ -1,10 +1,11 @@
 package NO_0349_Intersection_of_Two_Arrays;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int[] intersection_1(int[] nums1, int[] nums2) {
+    public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
@@ -24,7 +25,7 @@ class Solution {
         return res;
     }
 
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersection2(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
@@ -40,5 +41,27 @@ class Solution {
             res[i++] = num;
         }
         return res;
+    }
+
+    public int[] intersection3(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                if (k == 0 || res[k - 1] != nums1[i])
+                    res[k++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return Arrays.copyOf(res, k);
     }
 }
