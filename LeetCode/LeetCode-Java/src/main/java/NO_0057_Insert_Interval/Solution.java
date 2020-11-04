aadda71765125b8a1432863a584d81d3572a1b1c
liuanguang <liuanguang@cvte.com>
2020-11-04 09:59:36 +0800

    feat: NO_0057_Insert_Interval solved

--- LeetCode/LeetCode-Java/src/main/java/NO_0057_Insert_Interval/Solution.java
+++ LeetCode/LeetCode-Java/src/main/java/NO_0057_Insert_Interval/Solution.java
@@ -2,7 +2,7 @@ package NO_0057_Insert_Interval;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -10,37 +10,53 @@ import java.util.List;
  * https://leetcode-cn.com/problems/insert-interval/
  */
 class Solution {
-    // TODO: angus on 2020/11/4 
+
+    /**
+     * 将 newInterval 与 intervals 逐一比较，如果有交集，就合成新的 newInterval，
+     * 没有交集直接方到结果集中，最后把 newInterval 也加入，排序后输出
+     */
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<Integer> endpoints = new ArrayList<>();
+        List<int[]> res = new ArrayList<>();
         for (int[] interval : intervals) {
-            endpoints.add(interval[0]);
-            endpoints.add(interval[1]);
-        }
-        boolean hasOverlapping = false;
-        for (Iterator<Integer> it = endpoints.iterator(); it.hasNext(); ) {
-            Integer endPoint = it.next();
-            if (endPoint >= newInterval[0] && endPoint <= newInterval[1]) {
-                it.remove();
-                hasOverlapping = true;
+            if (hasOverlapping(interval, newInterval)) {
+                newInterval = mergeInterval(interval, newInterval);
+            } else {
+                res.add(interval);
             }
         }
-        if (!hasOverlapping) {
-            endpoints.add(newInterval[0]);
-            endpoints.add(newInterval[1]);
-        }
-        int[][] newIntervals = new int[endpoints.size() / 2][2];
-        for (int i = 0; i < endpoints.size(); i++) {
-            newIntervals[i / 2][i % 2] = endpoints.get(i);
-        }
-        return newIntervals;
+        res.add(newInterval);
+        res.sort(Comparator.comparingInt(i -> i[0]));
+        int[][] newIntervals = new int[res.size()][2];
+        return res.toArray(newIntervals);
+    }
+
+    /**
+     * 判断是否有交集
+     */
+    private boolean hasOverlapping(int[] i1, int[] i2) {
+        return i1[0] <= i2[1] && i1[1] >= i2[0];
+    }
+
+    /**
+     * 将两个 interval 合成一个
+     */
+    private int[] mergeInterval(int[] i1, int[] i2) {
+        return new int[]{
+                Math.min(i1[0], i2[0]),
+                Math.max(i1[1], i2[1])
+        };
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[][] intervals = new int[][]{{1, 3}, {6, 9}};
-        int[] newInterval = new int[]{2, 5};
+        int[] newInterval = new int[]{2, 4};
         int[][] res = solution.insert(intervals, newInterval);
         System.out.println(Arrays.deepToString(res));
+
+        int[][] intervals2 = new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+        int[] newInterval2 = new int[]{4, 8};
+        int[][] res2 = solution.insert(intervals2, newInterval2);
+        System.out.println(Arrays.deepToString(res2));
     }
 }
