c2c75940f8e707eb116c0248cb656e31653e6249
liuanguang <liuanguang@cvte.com>
2020-11-10 08:38:04 +0800

    feat: add NO_0031_Next_Permutation

---
+++ LeetCode/LeetCode-Java/src/main/java/NO_0031_Next_Permutation/Solution.java
@@ -0,0 +1,11 @@
+package NO_0031_Next_Permutation;
+
+/**
+ * 下一个排列
+ * https://leetcode-cn.com/problems/next-permutation/
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        // TODO: angus on 2020/11/10 
+    }
+}
