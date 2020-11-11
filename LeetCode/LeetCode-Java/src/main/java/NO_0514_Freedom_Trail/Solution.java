8e5d550334ca6497146f3de4f75ce8fc4f435d49
liuanguang <liuanguang@cvte.com>
2020-11-11 08:36:53 +0800

    feat: add NO_0514_Freedom_Trail

---
+++ LeetCode/LeetCode-Java/src/main/java/NO_0514_Freedom_Trail/Solution.java
@@ -0,0 +1,11 @@
+package NO_0514_Freedom_Trail;
+
+/**
+ * 自由之路
+ * https://leetcode-cn.com/problems/freedom-trail/
+ */
+class Solution {
+    public int findRotateSteps(String ring, String key) {
+
+    }
+}
