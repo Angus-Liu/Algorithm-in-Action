adbe56e073a56fe18110111c079845b63d0ddd87
liuanguang <liuanguang@cvte.com>
2021-05-23 10:47:16 +0800

    feat: update NO_0130_Surrounded_Regions

--- LeetCode/Java/src/main/java/NO_0130_Surrounded_Regions/Solution.java
+++ LeetCode/Java/src/main/java/NO_0130_Surrounded_Regions/Solution.java
@@ -1,9 +1,6 @@
 package NO_0130_Surrounded_Regions;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
@@ -11,7 +8,7 @@ class Solution {
     /**
      * 偏移量数组，用来计算指定坐标坐标相邻的上下左右的坐标
      */
-    private int[][] d = new int[][]{{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+    private final int[][] offsets = new int[][]{{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 
     /**
      * 访问标志数组，用来记录指定位置字母是否被访问过
@@ -21,22 +18,20 @@ class Solution {
     /**
      * key - x, value - y
      */
-    private List<Pair<Integer, Integer>> records = new ArrayList<>();
+    private final List<Pair<Integer, Integer>> records = new ArrayList<>();
 
-    private int r;
+    private int rows;
 
-    private int l;
+    private int cols;
 
     public void solve(char[][] board) {
         // 深度优先遍历、广度优先遍历均可，只要 O 所在的区域最后到不了边界，就是要被翻转为 X
-        if (board == null || board.length == 0) {
-            return;
-        }
-        r = board.length;
-        l = board[0].length;
-        visited = new boolean[r][l];
-        for (int y = 0; y < board.length; y++) {
-            for (int x = 0; x < board[y].length; x++) {
+        if (board == null || board.length == 0) return;
+        rows = board.length;
+        cols = board[0].length;
+        visited = new boolean[rows][cols];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
                 if (board[y][x] == 'O' && !visited[y][x]) {
                     if (dfs(board, x, y)) {
                         // 未访问到边界，需要翻转
@@ -55,14 +50,16 @@ class Solution {
         visited[y][x] = true;
         records.add(new Pair<>(x, y));
         // 当遍历到边界时，证明该区域未被包围
-        if (x == 0 || x == l - 1 || y == 0 || y == r - 1) {
+        if (x == 0 || x == cols - 1 || y == 0 || y == rows - 1) {
             surrounded = false;
         }
-        for (int i = 0; i < d.length; i++) {
-            int nextX = x + d[i][0];
-            int nextY = y + d[i][1];
-            if (nextX >= 0 && nextX < l && nextY >= 0 && nextY < r
-                    && board[nextY][nextX] == 'O' && !visited[nextY][nextX]) {
+        for (int[] o : offsets) {
+            int nextX = x + o[0];
+            int nextY = y + o[1];
+            if (nextX >= 0 && nextX < cols
+                    && nextY >= 0 && nextY < rows
+                    && board[nextY][nextX] == 'O'
+                    && !visited[nextY][nextX]) {
                 if (!dfs(board, nextX, nextY)) {
                     surrounded = false;
                 }
@@ -77,8 +74,11 @@ class Solution {
                 {'X', 'X', 'X', 'X'},
                 {'X', 'O', 'O', 'X'},
                 {'O', 'X', 'O', 'X'},
-                {'X', 'O', 'X', 'X'}};
+                {'X', 'O', 'X', 'X'}
+        };
         new Solution().solve(board);
-        System.out.println(Arrays.deepToString(board));
+        for (char[] rows : board) {
+            System.out.println(rows);
+        }
     }
 }
