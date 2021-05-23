2af0e7960d06e92d2d9c64afd8cd1cc5c581d4e7
liuanguang <liuanguang@cvte.com>
2021-05-23 11:26:00 +0800

    feat: add NO_0746_Min_Cost_Climbing_Stairs

---
+++ LeetCode/Kotlin/src/main/kotlin/NO_0746_Min_Cost_Climbing_Stairs/Solution.kt
@@ -0,0 +1,11 @@
+package NO_0746_Min_Cost_Climbing_Stairs
+
+class Solution {
+    fun minCostClimbingStairs(cost: IntArray): Int {
+        return 0
+    }
+}
+
+fun main() {
+    Solution().minCostClimbingStairs(intArrayOf(1, 2))
+}
