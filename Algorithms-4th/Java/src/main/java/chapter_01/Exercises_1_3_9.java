5a93d168a64be4a3d9a6608d75625bc0fcc8b600
liuanguang <liuanguang@cvte.com>
2021-06-13 17:22:14 +0800

    feat: add Exercises_1_3_9

--- Algorithms-4th/Java/src/main/java/chapter_01/Exercises_1_3_9.java
+++ Algorithms-4th/Java/src/main/java/chapter_01/Exercises_1_3_9.java
@@ -6,6 +6,9 @@ import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Exercises 1.3.9
+ * 编写一段程序，从标准输入得到一个缺少左括号的表达式并打印出补全括号之后的中序表达式。
+ * 例如，给定输入：1+2)*3-4)*5-6)))
+ * 你的程序应该输出：((1+2)*((3-4)*(5-6)))
  */
 public class Exercises_1_3_9 {
     public static void main(String[] args) {
