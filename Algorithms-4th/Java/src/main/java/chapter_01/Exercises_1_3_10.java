f9dc06c17d051a14d3f860cf4774765c15569a8d
liuanguang <liuanguang@cvte.com>
2021-06-13 22:41:57 +0800

    feat: add Exercises_1_3_10

---
+++ Algorithms-4th/Java/src/main/java/chapter_01/Exercises_1_3_10.java
@@ -0,0 +1,33 @@
+package chapter_01;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercises 1.3.10
+ */
+public class Exercises_1_3_10 {
+    /**
+     * // TODO: angus on 2021/6/13 待优化
+     */
+    public static void main(String[] args) {
+        Stack<String> vars = new Stack<>();
+        Stack<String> ops = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String str = StdIn.readString();
+            if (str.equals("q")) break;
+            if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+                ops.push(str);
+            } else if (str.equals(")")) {
+                String s1 = vars.pop();
+                String s2 = vars.pop();
+                String op = ops.pop();
+                vars.push("(" + s2 + " " + s1 + " " + op + ")");
+            } else {
+                vars.push(str);
+            }
+        }
+        StdOut.println(vars.pop());
+    }
+}
