3736bf08a7c1c2bb72d2a910afa18b5c6b48625e
Angus Liu <angus.liu96@gmail.com>
2022-10-26 10:35:17 +0800

    feat: update NO_0590_N_ary_Tree_Postorder_Traversal

--- LeetCode/Java/src/main/java/NO_0590_N_ary_Tree_Postorder_Traversal/Solution.java
+++ LeetCode/Java/src/main/java/NO_0590_N_ary_Tree_Postorder_Traversal/Solution.java
@@ -1,12 +1,10 @@
 package NO_0590_N_ary_Tree_Postorder_Traversal;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
 
-    private List<Integer> res = new ArrayList<>();
+    private final List<Integer> res = new ArrayList<>();
 
     /**
      * 递归实现
@@ -24,22 +22,55 @@ class Solution {
     /**
      * 迭代实现（借助栈）
      */
-    public List<Integer> postorder_2(Node root) {
-        LinkedList<Node> stack = new LinkedList<>();
-        if (root != null) {
-            stack.push(root);
-        }
+    public List<Integer> postorder2(Node root) {
+        if (root == null) return res;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.addLast(root);
         while (!stack.isEmpty()) {
-            Node node = stack.peek();
+            Node node = stack.getLast();
             if (node.children == null) {
-                res.add(stack.pop().val);
+                res.add(stack.removeLast().val);
             } else {
-                // for (int i = node.children.size() - 1; i >= 0; i--) {
-                //     stack.push(node.children.get(i));
-                // }
-                stack.addAll(0, node.children);
+                // 注意，靠右的孩子先入栈
+                for (int i = node.children.size() - 1; i >= 0; i--) {
+                    stack.addLast(node.children.get(i));
+                }
                 // 表示已遍历过其子节点（不太好，直接改变原节点信息了，但比较简单）
+                // 可改为用一个 map 记录当前节点的孩子全被访问过
                 node.children = null;
+            }
+        }
+        return res;
+    }
+
+    static class EnhanceNode extends Node {
+        boolean childrenHasVisited;
+
+        public EnhanceNode(Node node) {
+            this.val = node.val;
+            this.children = node.children;
+            this.childrenHasVisited = node.children == null || node.children.size() == 0;
+        }
+    }
+
+    /**
+     * 迭代实现（借助栈）
+     */
+    public List<Integer> postorder3(Node root) {
+        if (root == null) return res;
+        Deque<EnhanceNode> stack = new ArrayDeque<>();
+        stack.addLast(new EnhanceNode(root));
+        while (!stack.isEmpty()) {
+            EnhanceNode node = stack.getLast();
+            if (node.childrenHasVisited) {
+                res.add(stack.removeLast().val);
+            } else {
+                // 注意，靠右的孩子先入栈
+                for (int i = node.children.size() - 1; i >= 0; i--) {
+                    stack.addLast(new EnhanceNode(node.children.get(i)));
+                }
+                // 表示已遍历过其子节点
+                node.childrenHasVisited = true;
             }
         }
         return res;
