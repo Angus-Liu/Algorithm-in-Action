c83b3e311960640979daf767ab1627cbd3965926
Angus Liu <angus.liu96@gmail.com>
2023-01-07 17:16:25 +0800

    feat: update  Exercises_1_1_6

---
+++ Algorithms-4th/Java/src/main/java/chapter_01/Exercises_1_1_6.java
@@ -0,0 +1,16 @@
+package chapter_01;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercises_1_1_6 {
+    public static void main(String[] args) {
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i <= 15; i++) {
+            // 输出为斐波那契数
+            StdOut.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+}
