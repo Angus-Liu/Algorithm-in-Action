1f7a77d8bef2357e03d1bc21451d66bc46d982cb
Angus Liu <angus.liu96@gmail.com>
2023-01-07 18:02:51 +0800

    feat: add Matrix

---
+++ Algorithms-4th/Java/src/main/java/chapter_01/Matrix.java
@@ -0,0 +1,38 @@
+package chapter_01;
+
+public class Matrix {
+    /**
+     * 向量点乘
+     */
+    public static double dot(double[] x, double[] y) {
+        return 0.0;
+    }
+
+    /**
+     * 矩阵和矩阵之积
+     */
+    public static double[][] mult(double[][] a, double[][] b) {
+        return null;
+    }
+
+    /**
+     * 转置矩阵
+     */
+    public static double[][] transpose(double[][] a) {
+        return null;
+    }
+
+    /**
+     * 矩阵和向量之积
+     */
+    public static double[] mult(double[][] a, double[] x) {
+        return null;
+    }
+
+    /**
+     * 向量和矩阵之积
+     */
+    public static double[] mult(double[] y, double[][] a) {
+       return null;
+    }
+}
