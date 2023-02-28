ad511d29e3389a5926d098c1703043dcb8348e7e
Angus Liu <angus.liu96@gmail.com>
2023-02-28 08:37:22 +0800

    feat: update NO_0561_Array_Partition

--- LeetCode/Java/src/main/java/NO_0561_Array_Partition/Solution.java
+++ LeetCode/Java/src/main/java/NO_0561_Array_Partition/Solution.java
@@ -1,13 +1,12 @@
-package NO_0561_Array_Partition_I;
+package NO_0561_Array_Partition;
 
 import java.util.Arrays;
 
 class Solution {
     public int arrayPairSum(int[] nums) {
-        int len = nums.length;
         Arrays.sort(nums);
         int res = 0;
-        for (int i = 0; i < len; i += 2) {
+        for (int i = 0; i < nums.length; i += 2) {
             res += nums[i];
         }
         return res;
