817809982507ff65b04a3a48771f4a1a79cf111b
Angus Liu <angus.liu96@gmail.com>
2023-02-28 12:39:59 +0800

    feat: update NO_0119_Pascals_Triangle_II

--- LeetCode/Java/src/main/java/NO_0119_Pascals_Triangle_II/Solution.java
+++ LeetCode/Java/src/main/java/NO_0119_Pascals_Triangle_II/Solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
+    public List<Integer> getRow1(int rowIndex) {
         List<Integer> res = new ArrayList<>();
         res.add(1);
         for (int i = 0; i < rowIndex; i++) {
@@ -14,12 +14,25 @@ class Solution {
             }
             // 记得第一个元素是个特例
             res.add(0, 1);
-            System.out.println(res);
+        }
+        return res;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        for (int i = 0; i < rowIndex; i++) {
+            res.add(1); // 尾部为 1
+            for (int j = i; j > 0; j--) {
+                // 每一行由上一行得到
+                res.set(j, res.get(j - 1) + res.get(j));
+            }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        new Solution().getRow(3);
+        List<Integer> row = new Solution().getRow(3);
+        System.out.println("row = " + row);
     }
 }
