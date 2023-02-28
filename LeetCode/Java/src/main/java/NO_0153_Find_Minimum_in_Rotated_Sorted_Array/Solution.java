35587f8b2c242dfe3d030169e9b08375def61919
Angus Liu <angus.liu96@gmail.com>
2023-02-28 14:49:20 +0800

    feat: update NO_0153_Find_Minimum_in_Rotated_Sorted_Array

--- LeetCode/Java/src/main/java/NO_0153_Find_Minimum_in_Rotated_Sorted_Array/Solution.java
+++ LeetCode/Java/src/main/java/NO_0153_Find_Minimum_in_Rotated_Sorted_Array/Solution.java
@@ -2,17 +2,15 @@ package NO_0153_Find_Minimum_in_Rotated_Sorted_Array;
 
 class Solution {
     public int findMin(int[] nums) {
-        int n = nums.length;
-        int l = 0, r = n - 1;
-        while (l < r) {
+        int l = 0, r = nums.length - 1;
+        while (l < r && nums[l] >= nums[r]) {
             int m = (l + r) / 2;
-            if (nums[l] < nums[r]) break; // 剪枝
             if (nums[m] < nums[r]) {
                 r = m; // 注意，不能直接赋值为 m + 1，因原 m 可能为最终答案
             } else {
                 l = m + 1;
             }
-            // System.out.printf("l = %d, r = %d, m = %d\n", l, r, m);
+             System.out.printf("l = %d, r = %d, m = %d\n", l, r, m);
         }
         return nums[l];
     }
--- LeetCode/Java/src/main/java/NO_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java
+++ LeetCode/Java/src/main/java/NO_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java
@@ -2,21 +2,25 @@ package NO_0154_Find_Minimum_in_Rotated_Sorted_Array_II;
 
 class Solution {
     public int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
         int l = 0, r = nums.length - 1;
-        while (l <= r) {
+        while (l < r && nums[l] >= nums[r]) {
             int m = l + (r - l) / 2;
-            if (nums[m] > nums[r]) {
-                l = m + 1;
-            } else if (nums[l] > nums[m]) {
+            if (nums[m] < nums[r]) {
                 r = m;
+            } else if (nums[m] > nums[r]) {
+                l = m + 1;
             } else {
-                // nums[l] <= nums[m] <= nums[r]，排除重复值
+                // nums[m] == nums[r]，排除重复值
                 r -= 1;
             }
         }
         return nums[l];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 1, 3, 3};
+        int min = solution.findMin(nums);
+        System.out.println("min = " + min);
     }
 }
