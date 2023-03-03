326152d6509615257b31b49a089e70397c69256f
Angus Liu <angus.liu96@gmail.com>
2023-03-03 22:37:33 +0800

    feat: update NO_0003_Longest_Substring_Without_Repeating_Characters

--- LeetCode/Java/src/main/java/NO_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
+++ LeetCode/Java/src/main/java/NO_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -1,31 +1,28 @@
 package NO_0003_Longest_Substring_Without_Repeating_Characters;
 
 class Solution {
+
+    /**
+     * 滑动窗口解法
+     */
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        // 滑动窗口解法
-        // freq 用于存储滑动窗口中某个字符出现的次数
-        int freq[] = new int[256];
-        // 滑动窗口为 s[l, r]
-        int l = 0;
-        // r = -1，表示窗口初始大小为 0
-        int r = -1;
-        int len = 0;
+        if (s == null || s.length() == 0) return 0;
+        // flags 用于表明滑动窗口是否包含某个字符
+        boolean[] flags = new boolean[256];
+        // 滑动窗口为 s[l, r]，r = -1，表示窗口初始大小为 0
+        int l = 0, r = -1;
+        int maxLen = 0;
         while (l < s.length()) {
-            if (r + 1 < s.length() && freq[s.charAt(r + 1)] == 0) {
+            if (r + 1 < s.length() && !flags[s.charAt(r + 1)]) {
                 // 右边界的下一个元素未在窗口中出现过，就加入到窗口中
-                r++;
-                freq[s.charAt(r)]++;
+                flags[s.charAt(++r)] = true;
             } else {
                 // 否则，就将窗口右移动，直到把重复元素移出去为止
-                freq[s.charAt(l)]--;
-                l++;
+                flags[s.charAt(l++)] = false;
             }
-            len = r - l + 1 > len ? r - l + 1 : len;
+            maxLen = Math.max(r - l + 1, maxLen);
         }
-        return len;
+        return maxLen;
     }
 
     public static void main(String[] args) {
