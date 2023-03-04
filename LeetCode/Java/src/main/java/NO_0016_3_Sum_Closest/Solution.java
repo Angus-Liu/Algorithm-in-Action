ba3b64e4c79e36aa7e63e01634aa1c4c59ab1621
Angus Liu <angus.liu96@gmail.com>
2023-03-04 11:48:06 +0800

    feat: update NO_0016_3_Sum_Closest

--- LeetCode/Java/src/main/java/NO_0016_3_Sum_Closest/Solution.java
+++ LeetCode/Java/src/main/java/NO_0016_3_Sum_Closest/Solution.java
@@ -6,33 +6,27 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         // 先对 nums 做预处理，排序
         Arrays.sort(nums);
-        // sum 为三数之和，diff 则对应其与 target 的接近程度（取绝对值）
-        int sum = nums[0] + nums[1] + nums[2];
-        int diff = Math.abs(target - sum);
-
-        for (int i = 0; i < nums.length; i++) {
+        // cloSum 为最接近的三数之和，minDiff 则对应其与 target 的接近程度（取绝对值）
+        int cloSum = Integer.MAX_VALUE, minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - 2; i++) {
             // 采用双指针法
-            int l = i + 1, r = nums.length - 1;
-            while (l < r) {
-                int s = nums[i] + nums[l] + nums[r];
-                int d = target - s;
-                if (s == target) {
-                    return s;
+            for (int l = i + 1, r = nums.length - 1; l < r; ) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == target) return sum;
+                int diff = target - sum;
+                if (Math.abs(diff) < minDiff) {
+                    minDiff = Math.abs(diff);
+                    cloSum = sum;
+                }
+                // 根据接近情况移动指针
+                if (diff > 0) {
+                    l++;
                 } else {
-                    if (Math.abs(d) < diff) {
-                        diff = Math.abs(d);
-                        sum = s;
-                    }
-                    // 根据接近情况移动指针
-                    if (d > 0) {
-                        l++;
-                    } else {
-                        r--;
-                    }
+                    r--;
                 }
             }
         }
-        return sum;
+        return cloSum;
     }
 
     public static void main(String[] args) {
