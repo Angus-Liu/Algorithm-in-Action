355dc90e6911656fc4004379303ef92ecc944e0a
Angus Liu <angus.liu96@gmail.com>
2023-03-04 11:19:32 +0800

    feat: update NO_0086_Partition_List

--- LeetCode/Java/src/main/java/NO_0086_Partition_List/Solution.java
+++ LeetCode/Java/src/main/java/NO_0086_Partition_List/Solution.java
@@ -1,33 +1,7 @@
 package NO_0086_Partition_List;
 
 
-
 class Solution {
-    /**
-     * 此方法没有保留两个分区中每个节点的初始相对位置，不可行
-     */
-    public ListNode partition1(ListNode head, int x) {
-        ListNode pre = head;
-        ListNode cur = pre.next;
-        while (cur != null && cur.val >= x) {
-            cur = cur.next;
-        }
-        while (pre != null && cur != null) {
-            if (pre.val >= x) {
-                int temp = pre.val;
-                pre.val = cur.val;
-                cur.val = temp;
-                pre = pre.next;
-                while (cur != null && cur.val >= x) {
-                    cur = cur.next;
-                }
-            } else {
-                pre = pre.next;
-            }
-        }
-        return head;
-    }
-
     public ListNode partition(ListNode head, int x) {
         ListNode dummyHead1 = new ListNode(0);
         ListNode dummyHead2 = new ListNode(0);
@@ -36,17 +10,15 @@ class Solution {
         while (head != null) {
             if (head.val < x) {
                 node1.next = head;
-                head = head.next;
                 node1 = node1.next;
-                node1.next = null;
             } else {
                 node2.next = head;
-                head = head.next;
                 node2 = node2.next;
-                node2.next = null;
             }
+            head = head.next;
         }
         node1.next = dummyHead2.next;
+        node2.next = null;
         return dummyHead1.next;
     }
 
