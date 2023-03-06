47943cd188aeb8612a61ecf896c45bd23f428495
Angus Liu <angus.liu96@gmail.com>
2023-03-06 16:58:48 +0800

    feat: update NO_0554_Brick_Wall

--- LeetCode/Java/src/main/java/NO_0554_Brick_Wall/Solution.java
+++ LeetCode/Java/src/main/java/NO_0554_Brick_Wall/Solution.java
@@ -20,4 +20,18 @@ class Solution {
         // 砖的行数减去所有列中缝隙出现次数最多的一列的缝隙数，即使穿过的砖块数量最少
         return wall.size() - gapCnt.values().stream().max(Integer::compareTo).orElse(0);
     }
+
+public int leastBricks2(List<List<Integer>> wall) {
+    // key - 列号，value - 该列中缝隙出现的次数
+    Map<Integer, Integer> gapCnt = new HashMap<>();
+    int maxCnt = 0;
+    for (List<Integer> bricks : wall) {
+        for (int i = 0, col = 0; i < bricks.size() - 1; i++) {
+            col += bricks.get(i);
+            maxCnt = Math.max(maxCnt, gapCnt.merge(col, 1, Integer::sum));
+        }
+    }
+    // 砖的行数减去所有列中缝隙出现次数最多的一列的缝隙数，即使穿过的砖块数量最少
+    return wall.size() - maxCnt;
+}
 }
