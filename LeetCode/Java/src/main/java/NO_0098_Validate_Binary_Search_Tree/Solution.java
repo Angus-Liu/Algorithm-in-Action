41cab7c9baf677f6bac5a3b4e65d417e623ce9ef
Angus Liu <angus.liu96@gmail.com>
2023-03-07 09:02:41 +0800

    feat: update NO_0098_Validate_Binary_Search_Tree

--- LeetCode/Java/src/main/java/NO_0098_Validate_Binary_Search_Tree/Solution.java
+++ LeetCode/Java/src/main/java/NO_0098_Validate_Binary_Search_Tree/Solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST1(TreeNode root) {
         // 利用二分搜索树的中序遍历来判断
         List<Integer> list = new ArrayList<>();
         inOrder(root, list);
@@ -25,17 +25,13 @@ class Solution {
         inOrder(node.right, list);
     }
 
-    public boolean isValidBST2(TreeNode root) {
-        return isValidBST2(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
     }
 
-    public boolean isValidBST2(TreeNode root, long max, long min) {
-        if (root == null) {
-            return true;
-        } else if (root.val <= min || root.val >= max) {
-            return false;
-        } else {
-            return isValidBST2(root.left, root.val, min) && isValidBST2(root.right, max, root.val);
-        }
+    private boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) return true;
+        if (node.val <= lower || node.val >= upper) return false;
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
     }
 }
