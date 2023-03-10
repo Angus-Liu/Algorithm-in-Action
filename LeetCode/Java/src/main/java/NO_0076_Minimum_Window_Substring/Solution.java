4eba63c22f63de6e38eb714eeed10e55bb6df382
Angus Liu <angus.liu96@gmail.com>
2023-03-10 13:08:02 +0800

    feat: update NO_0076_Minimum_Window_Substring

--- LeetCode/Java/src/main/java/NO_0076_Minimum_Window_Substring/Solution.java
+++ LeetCode/Java/src/main/java/NO_0076_Minimum_Window_Substring/Solution.java
@@ -1,78 +1,43 @@
 package NO_0076_Minimum_Window_Substring;
 
 class Solution {
-    public String minWindow(String s, String t) {
-        if (s == null || s.length() == 0 || t == null || t.length() == 0 || t.length() > s.length()) {
-            return "";
-        }
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        // 滑动窗口解法
-        // tFreq，windowFreq 分别用于存储 p 和滑动窗口中某个字符出现的次数
-        int[] tFreq = new int[256];
-        int[] windowFreq = new int[256];
-        // 初始化 tFreq
-        for (int i = 0; i < tChars.length; i++) {
-            tFreq[tChars[i]]++;
-        }
-        // minSize，minL，minR 用于记录最小子串的位置信息
-        int minSize = sChars.length + 1;
-        int minL = -1;
-        int minR = -1;
-        // 滑动窗口左右边界，初始值为空集
-        int l = 0;
-        int r = -1;
-        while (l < sChars.length - tChars.length + 1) {
-            if (r - l + 1 < tChars.length) {
-                if (r + 1 < sChars.length) {
-                    windowFreq[sChars[++r]]++;
-                } else {
-                    break;
-                }
-            } else {
-                if (isMatch(tChars, windowFreq, tFreq)) {
-                    if (minSize > r - l + 1) {
-                        minSize = r - l + 1;
-                        minL = l;
-                        minR = r;
-                    }
-                    // 匹配上之后，窗口左边界右移，缩小窗口大小
-                    windowFreq[sChars[l++]]--;
-                } else {
-                    if (r + 1 < sChars.length) {
-                        windowFreq[sChars[++r]]++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        if (minL == -1 || minR == -1) {
-            return "";
-        }
-        return s.substring(minL, minR + 1);
-    }
-
     /**
-     * 检查 t 中所有字符是否都在滑动窗口中
-     *
-     * @param tChars
-     * @param windowFreq
-     * @param tFreq
-     * @return
+     * 滑动窗口解法
      */
-    private boolean isMatch(char[] tChars, int[] windowFreq, int[] tFreq) {
-//        System.out.println("----------");
-//        for (int i = 0; i < tChars.length; i++) {
-//            System.out.println("freq[" + tChars[i] + "]: " + windowFreq[tChars[i]]);
-//        }
-        for (int i = 0; i < tChars.length; i++) {
-            if (windowFreq[tChars[i]] < tFreq[tChars[i]]) {
-                return false;
-            }
-        }
-        return true;
+public String minWindow(String s, String t) {
+    // tFreq 存储 t 中某个字符出现的次数
+    int[] tFreq = new int[128];
+    for (char c : t.toCharArray()) {
+        tFreq[c]++;
     }
+    // start，minLen 用于记录最小子串的位置信息
+    int start = 0, minLen = Integer.MAX_VALUE;
+    // wFreq 记录 t 中字符在滑动窗口中出现的次数
+    int[] wFreq = new int[128];
+    // tInWinCnt 表示当前窗口包含 t 中的字符的总数
+    int tInWinCnt = 0;
+    char[] sChars = s.toCharArray();
+    // left，right 为滑动窗口左右边界
+    for (int left = 0, right = 0; right < s.length(); right++) {
+        char rc = sChars[right];
+        if (tFreq[rc] > 0) { // 如果是 t 中字符
+            if (++wFreq[rc] <= tFreq[rc]) tInWinCnt++;
+        }
+        while (tInWinCnt == t.length()) { // 当前窗口包含 t 中所有字符
+            int curLen = right - left + 1;
+            if (curLen < minLen) {
+                start = left;
+                minLen = curLen;
+            }
+            char lc = sChars[left];
+            if (tFreq[lc] > 0) { // 如果是 t 中字符
+                if (--wFreq[lc] < tFreq[lc]) tInWinCnt--;
+            }
+            left++;
+        }
+    }
+    return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+}
 
     public static void main(String[] args) {
         Solution solution = new Solution();
