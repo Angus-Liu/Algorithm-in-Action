7b3e16ae972e481e04d3bfb8e0d502d142c779e5
Angus Liu <angus.liu96@gmail.com>
2023-03-12 13:04:36 +0800

    feat: update some

--- LeetCode/Java/src/main/java/NO_6316_Rearrange_Array_to_Maximize_Prefix_Score/Solution.java
+++ LeetCode/Java/src/main/java/NO_6316_Rearrange_Array_to_Maximize_Prefix_Score/Solution.java
@@ -4,21 +4,12 @@ import java.util.Arrays;
 
 class Solution {
     public int maxScore(int[] nums) {
-        int n = nums.length;
-        long posSum = 0;
-        int negCnt = 0;
-        for (int num : nums) {
-            if (num > 0) {
-                posSum += num;
-            } else if (num < 0) {
-                negCnt++;
-            }
-        }
-        if (posSum <= 0) return 0;
-        int res = n - negCnt;
         Arrays.sort(nums);
-        for (int i = negCnt - 1; i >= 0; i--) {
-            if ((posSum += nums[i]) > 0) res++;
+        int res = 0;
+        long posSum = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            posSum += nums[i];
+            if (posSum > 0) res++;
         }
         return res;
     }
@@ -27,7 +18,5 @@ class Solution {
         Solution solution = new Solution();
         int res = solution.maxScore(new int[]{2, -1, 0, 1, -3, 3, -3});
         System.out.println("res = " + res);
-
-        // 1 1 1 1 0 0 -1 -1 -1 -1
     }
 }
