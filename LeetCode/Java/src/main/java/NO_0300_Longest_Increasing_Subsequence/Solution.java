a9be3da343e4bb9b231f451d2ac610cbf9394fa9
Angus Liu <angus.liu96@gmail.com>
2023-03-14 12:56:27 +0800

    feat: update NO_0300_Longest_Increasing_Subsequence

--- LeetCode/Java/src/main/java/NO_0300_Longest_Increasing_Subsequence/Solution.java
+++ LeetCode/Java/src/main/java/NO_0300_Longest_Increasing_Subsequence/Solution.java
@@ -10,6 +10,7 @@ class Solution {
         }
         // memo[i] 表示以 nums[i] 为结尾的最长上升子序列的长度
         int[] memo = new int[nums.length];
+        int res = 1;
         // 初始化时，每位数字都是长度为 1 的最长上升子序列
         Arrays.fill(memo, 1);
         for (int i = 1; i < nums.length; i++) {
@@ -18,11 +19,24 @@ class Solution {
                     memo[i] = Math.max(memo[i], 1 + memo[j]);
                 }
             }
-        }
-        int res = 1;
-        for (int i = 0; i < memo.length; i++) {
             res = Math.max(res, memo[i]);
         }
         return res;
+    }
+
+    public int lengthOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int maxLen = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (dp[i] == 0) dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
     }
 }
