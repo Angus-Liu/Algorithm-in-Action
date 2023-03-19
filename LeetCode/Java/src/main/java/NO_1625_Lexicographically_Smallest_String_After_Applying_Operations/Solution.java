c74984b9d49a61101fe554e7a0d46bfcdae2d296
Angus Liu <angus.liu96@gmail.com>
2023-03-19 11:36:11 +0800

    feat: update NO_1625_Lexicographically_Smallest_String_After_Applying_Operations

---
+++ LeetCode/Java/src/main/java/NO_1625_Lexicographically_Smallest_String_After_Applying_Operations/Solution.java
@@ -0,0 +1,7 @@
+package NO_1625_Lexicographically_Smallest_String_After_Applying_Operations;
+
+class Solution {
+    public String findLexSmallestString(String s, int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+}
