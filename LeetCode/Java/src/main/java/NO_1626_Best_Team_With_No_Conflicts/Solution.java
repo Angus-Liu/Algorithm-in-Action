73a6835fbc2a69125db19ccf0e028e5b666fec3b
Angus Liu <angus.liu96@gmail.com>
2023-03-22 20:07:54 +0800

    feat: update NO_1626_Best_Team_With_No_Conflicts

---
+++ LeetCode/Java/src/main/java/NO_1626_Best_Team_With_No_Conflicts/Solution.java
@@ -0,0 +1,26 @@
+package NO_1626_Best_Team_With_No_Conflicts;
+
+import java.util.Arrays;
+
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = scores.length;
+        int[][] people = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            people[i] = new int[]{scores[i], ages[i]};
+        }
+        Arrays.sort(people, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (people[j][1] <= people[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += people[i][0];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
