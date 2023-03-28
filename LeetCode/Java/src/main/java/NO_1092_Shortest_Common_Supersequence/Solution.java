37ea3ee3cdb5414846ad28dfccb74c661a194c27
Angus Liu <angus.liu96@gmail.com>
2023-03-28 12:51:16 +0800

    feat: add NO_1092_Shortest_Common_Supersequence

---
+++ LeetCode/Java/src/main/java/NO_1092_Shortest_Common_Supersequence/Solution.java
@@ -0,0 +1,13 @@
+package NO_1092_Shortest_Common_Supersequence;
+
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String res = solution.shortestCommonSupersequence("abac", "aba");
+        System.out.println("res = " + res);
+    }
+}
