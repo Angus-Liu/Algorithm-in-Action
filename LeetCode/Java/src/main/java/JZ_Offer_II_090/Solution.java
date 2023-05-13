8d258e18a9836daddac399ad3d96b3626da54d81
Angus Liu <angus.liu96@gmail.com>
2023-05-13 16:50:25 +0800

    feat: update JZ_Offer_II_090

---
+++ LeetCode/Java/src/main/java/JZ_Offer_II_090/Solution.java
@@ -0,0 +1,28 @@
+package JZ_Offer_II_090;
+
+class Solution {
+    /**
+     * 动态规划，在 198 号问题基础上修改
+     */
+    public int rob(int[] nums) {
+        int n = nums.length;
+        return switch (n) {
+            case 0 -> 0;
+            case 1 -> nums[0];
+            case 2 -> Math.max(nums[0], nums[1]);
+            // 考虑不抢劫 n-1 号房间和不抢劫 0 号房间的情况
+            default -> Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
+        };
+    }
+
+    private int rob(int[] nums, int start, int end) {
+        int pre = nums[start];
+        int cur = Math.max(pre, nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int tmp = cur;
+            cur = Math.max((pre + nums[i]), cur);
+            pre = tmp;
+        }
+        return cur;
+    }
+}
