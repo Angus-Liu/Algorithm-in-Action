c7b595668046ca756c49c3757f9a38e276295394
Angus Liu <angus.liu96@gmail.com>
2023-06-03 16:31:36 +0800

    feat: update JZ_Offer_II_068

---
+++ LeetCode/Java/src/main/java/JZ_Offer_II_068/Solution.java
@@ -0,0 +1,19 @@
+package JZ_Offer_II_068;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return r + 1;
+    }
+}
