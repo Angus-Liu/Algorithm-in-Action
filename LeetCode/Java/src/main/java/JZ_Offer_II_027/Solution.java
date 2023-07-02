be8179f6a4fb592cbb3545453b77ea055f2edc54
Angus Liu <angus.liu96@gmail.com>
2023-07-02 14:51:53 +0800

    feat: update JZ_Offer_II_027

---
+++ LeetCode/Java/src/main/java/JZ_Offer_II_027/Solution.java
@@ -0,0 +1,35 @@
+package JZ_Offer_II_027;
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode l1 = head;
+        ListNode l2 = reverse(slow.next);
+        slow.next = null;
+        while (l1 != null && l2 != null) {
+            if (l1.val != l2.val) return false;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
--- LeetCode/Java/src/main/java/NO_0234_Palindrome_Linked_List/Solution.java
+++ LeetCode/Java/src/main/java/NO_0234_Palindrome_Linked_List/Solution.java
@@ -1,7 +1,6 @@
 package NO_0234_Palindrome_Linked_List;
 
 
-
 class Solution {
     public boolean isPalindrome(ListNode head) {
         // 要实现 O(n) 的时间复杂度和 O(1) 的空间复杂度，需要翻转后半部分
@@ -11,12 +10,12 @@ class Solution {
         ListNode fast = head;
         ListNode slow = head;
         // 根据快慢指针，找到链表的中点
-        while(fast.next != null && fast.next.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
         slow = reverse(slow.next);
-        while(slow != null) {
+        while (slow != null) {
             if (head.val != slow.val) {
                 return false;
             }
@@ -26,7 +25,7 @@ class Solution {
         return true;
     }
 
-    private ListNode reverse(ListNode head){
+    private ListNode reverse(ListNode head) {
         // 递归到最后一个节点，返回新的新的头结点
         if (head.next == null) {
             return head;
