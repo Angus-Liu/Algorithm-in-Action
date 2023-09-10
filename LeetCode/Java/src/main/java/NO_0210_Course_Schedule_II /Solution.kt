e37747a7e2b78ddf5f2cd4c63d475dcfac4c1750
Angus Liu <angus.liu96@gmail.com>
2023-09-10 14:05:56 +0800

    feat: update NO_0210_Course_Schedule_II

---
+++ LeetCode/Java/src/main/java/NO_0210_Course_Schedule_II /Solution.kt
@@ -0,0 +1,23 @@
+package `NO_0210_Course_Schedule_II `
+
+class Solution {
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+        val preMap = HashMap<Int, MutableList<Int>>()
+        prerequisites.forEach {
+            preMap.getOrPut(it[0]) { ArrayList() }.add(it[1])
+        }
+        val visited = BooleanArray(numCourses)
+        val res = IntArray(numCourses)
+        var ri = 0
+        for (i in 0..<numCourses) {
+            for (j in 0..<numCourses) {
+                if (!visited[j] && preMap[j]?.all { visited[it] } != false) {
+                    res[ri++] = j
+                    visited[j] = true
+                }
+                if (ri == numCourses) return res
+            }
+        }
+        return IntArray(0)
+    }
+}
