6f90fa815043bcd2610cc12086f25317f84c2deb
Angus Liu <angus.liu96@gmail.com>
2024-06-12 23:36:40 +0800

    feat: add 312

---
+++ LeetCode/CPP/0312-Burst-Balloons/Solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
