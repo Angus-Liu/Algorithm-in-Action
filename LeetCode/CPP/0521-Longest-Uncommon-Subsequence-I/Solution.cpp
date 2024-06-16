1e8979f5c3d8f9b91060cd30c015dc97369e7cb1
Angus Liu <angus.liu96@gmail.com>
2024-06-16 10:20:50 +0800

    feat: add 521

---
+++ LeetCode/CPP/0521-Longest-Uncommon-Subsequence-I/Solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if (a == b) return -1;
+        return max(a.size(), b.size());
+    }
+};
